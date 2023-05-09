2ac81a9a5e1af793166a7e79ac8c0a4c137c425b
huangtianyun <352496193@qq.com>
2023-05-09 16:04:10 +0800

    add tread

--- lib/dialog/create_tread_dialog.dart
+++ lib/dialog/create_tread_dialog.dart
@@ -1,5 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:metting/tool/view_tools.dart';
 
 class CreateTreadDialog {
+  int _itemIndex = 0;
 
-
+  void showDialog() {
+    Get.dialog(
+        Column(
+      children: [Image.asset(getImagePath('name')), Container()],
+    ));
+  }
 }
